3d63cacee93e254bf27ebd9369a3ce1707066766
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-02-24 22:34:37 +0530

    Create Contest.py

---
+++ Contest.py
@@ -0,0 +1,3 @@
+x, y = map(int, input().split())  # taking input of X and Y
+unattempted = x - y              # calculating the number of unattempted problems
+print(unattempted)               # printing the result
