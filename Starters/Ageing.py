29d264accdb4877f90e4cd53240415cc5b16b6b4
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-03-01 20:50:37 +0530

    Create Ageing.py

---
+++ Starters/Ageing.py
@@ -0,0 +1,7 @@
+# cook your dish here
+t = int(input()) #no_of_test_cases
+
+for i in range(t):
+    x = int(input()) #age_of_Chef
+    chefina_age = x-10 #diff_b/w chef and chefina age remains constant
+    print(chefina_age) #print chefina age when chef is X years NotImplemented
