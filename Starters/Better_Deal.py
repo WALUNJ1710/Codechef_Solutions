36a6052ed9291b8b955e1521db9b835df57900d2
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-03-01 21:04:38 +0530

    Update Better_Deal.py

--- Starters/Better_Deal.py
+++ Starters/Better_Deal.py
@@ -2,7 +2,7 @@ t = int(input())
 for i in range(t):
     a,b = map(int,input().split())
     price1 = 100-a
-    price2 = 200-b
+    price2 = 200-(b*2)
     if price1 < price2:
         print("FIRST")
     elif price2 < price1:
